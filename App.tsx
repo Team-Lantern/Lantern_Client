a1ae2f14d87f6fa2d8d2adea2e7c96c7a3367b8b
yujinjeon12 <tamittu@naver.com>
2024-12-20 05:38:51 +0900

    refactor: 네비게이션 로직을 RootStack 컴포넌트로 분리 #5

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import colors from './src/constants/colors';
-import OnboardingStack from './src/navigation/OnboardingStack';
-
-const Stack = createNativeStackNavigator();
+import RootStack from './src/navigation/RootStack';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="OnboardingStack"
-          component={OnboardingStack}
-        />
-      </Stack.Navigator>
+      <RootStack />
     </NavigationContainer>
   );
 }
---
+++ src/navigation/RootStack.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import OnboardingStack from './OnboardingStack';
+
+const Stack = createNativeStackNavigator();
+
+const RootStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="Onboarding"
+        component={OnboardingStack}
+      />
+    </Stack.Navigator>
+  )
+}
+
+export default RootStack
